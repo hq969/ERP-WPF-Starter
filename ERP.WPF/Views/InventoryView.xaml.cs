649d94b013dd7c7d05dca99c9f1503c626f2c17f
Harsh Sonkar <114162367+hq969@users.noreply.github.com>
2026-05-05 13:47:05 +0530

    Add InventoryView user control

---
+++ ERP.WPF/Views/InventoryView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ERP.WPF.Views
+{
+    public partial class InventoryView : UserControl
+    {
+        public InventoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
